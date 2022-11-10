62d2bb06444d36d7e6a4af36308b783618919672
Marfa Nikitina <marfik93@mail.ru>
2022-11-10 12:32:48 +0300

    10/11

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 from dotenv import load_dotenv
+from django.utils.translation import gettext_lazy as _
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -113,7 +114,11 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru'
+LANGUAGES = (
+    ('en-us', _('English')),
+    ('ru', _('Russian')),
+)
 TIME_ZONE = 'UTC'
 USE_I18N = True
 USE_L10N = True
@@ -131,78 +136,4 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-# LOCALE_PATHS = os.path.join(BASE_DIR, 'locale/')
-
-BOOTSTRAP4 = {
-
-    # The complete URL to the Bootstrap CSS file
-    # Note that a URL can be either a string,
-    # e.g. "https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css",
-    # or a dict like the default value below.
-    "css_url": {
-        "href": "https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css",
-        "integrity": "sha384-zCbKRCUGaJDkqS1kPbPd7TveP5iyJE0EjAuZQTgFLD2ylzuqKfdKlfG/eSrtxUkn",
-        "crossorigin": "anonymous",
-    },
-
-    # The complete URL to the Bootstrap bundle JavaScript file
-    "javascript_url": {
-        "url": "https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js",
-        "integrity": "sha384-fQybjgWLrvvRgtW6bFlB7jaZrFsaBXjsOMm/tB9LTS58ONXgqbR9W8oWht/amnpF",
-        "crossorigin": "anonymous",
-    },
-
-    # The complete URL to the Bootstrap CSS theme file (None means no theme)
-    "theme_url": None,
-
-    # The URL to the jQuery JavaScript file (full)
-    "jquery_url": {
-        "url": "https://code.jquery.com/jquery-3.5.1.min.js",
-        "integrity": "sha384-ZvpUoO/+PpLXR1lu4jmpXWu80pZlYUAfxl5NsBMWOEPSjUn/6Z/hRTt8+pR6L4N2",
-        "crossorigin": "anonymous",
-    },
-
-    # The URL to the jQuery JavaScript file (slim)
-    "jquery_slim_url": {
-        "url": "https://code.jquery.com/jquery-3.5.1.slim.min.js",
-        "integrity": "sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj",
-        "crossorigin": "anonymous",
-    },
-
-    # Put JavaScript in the HEAD section of the HTML document (only relevant if you use bootstrap4.html)
-    'javascript_in_head': False,
-
-    # Include jQuery with Bootstrap JavaScript False|falsy|slim|full (default=False)
-    # False - means tag bootstrap_javascript use default value - `falsy` and does not include jQuery)
-    'include_jquery': False,
-
-    # Label class to use in horizontal forms
-    'horizontal_label_class': 'col-md-3',
-
-    # Field class to use in horizontal forms
-    'horizontal_field_class': 'col-md-9',
-
-    # Set placeholder attributes to label if no placeholder is provided
-    'set_placeholder': True,
-
-    # Class to indicate required (better to set this in your Django form)
-    'required_css_class': '',
-
-    # Class to indicate error (better to set this in your Django form)
-    'error_css_class': 'is-invalid',
-
-    # Class to indicate success, meaning the field has valid input (better to set this in your Django form)
-    'success_css_class': 'is-valid',
-
-    # Renderers (only set these if you have studied the source and understand the inner workings)
-    'formset_renderers':{
-        'default': 'bootstrap4.renderers.FormsetRenderer',
-    },
-    'form_renderers': {
-        'default': 'bootstrap4.renderers.FormRenderer',
-    },
-    'field_renderers': {
-        'default': 'bootstrap4.renderers.FieldRenderer',
-        'inline': 'bootstrap4.renderers.InlineFieldRenderer',
-    },
-}
+LOCALE_PATHS = (BASE_DIR, 'locale/')
