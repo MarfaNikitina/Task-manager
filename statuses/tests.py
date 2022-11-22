239b990f2f2b970d88b7c2d0ff340905ea396697
Marfa Nikitina <marfik93@mail.ru>
2022-11-22 23:58:16 +0300

    add status.tests

--- statuses/tests.py
+++ statuses/tests.py
@@ -1,3 +1,52 @@
+from django.core.exceptions import ObjectDoesNotExist
 from django.test import TestCase
+from django.urls import reverse
+from statuses.models import Status
+from task_manager.utils import get_test_data, get_fixture_data
 
-# Create your tests here.
+
+class UserTests(TestCase):
+
+    def setUp(self):
+        self.status1 = Status.objects.create(
+            id=1,
+            name='Warning'
+        )
+        self.status2 = Status.objects.create(
+            id=2,
+            name='New'
+        )
+        self.users_list = reverse('statuses')
+        self.form_data = {'name': 'in process'}
+
+    def test_statuses_view(self):
+        response = self.client.get(reverse('statuses'))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'Warning')
+        self.assertContains(response, 'New')
+
+    def test_status_create(self):
+        response = self.client.get(reverse('create_status'))
+        self.assertEqual(response.status_code, 200)
+        response_post = self.client.post(reverse('create_status'), self.form_data)
+        self.assertRedirects(response_post, self.users_list)
+        self.assertTrue(Status.objects.get(id=3))
+
+    def test_status_update(self):
+        update_url = reverse('update_status', args=(self.status1.id,))
+        response = self.client.get(update_url)
+        self.assertEqual(response.status_code, 200)
+        response_post = self.client.post(update_url, self.form_data)
+        self.assertRedirects(response_post, self.users_list)
+        status = Status.objects.get(pk=1)
+        self.assertEqual(status.name, self.form_data['name'])
+
+    def test_delete_status(self):
+        delete_url = reverse('delete_status', args=(self.status1.id,))
+        response = self.client.get(delete_url)
+        self.assertEqual(response.status_code, 200)
+        response_post = self.client.post(delete_url)
+        self.assertRedirects(response_post, self.users_list)
+        with self.assertRaises(ObjectDoesNotExist):
+            Status.objects.get(id=self.status1.id)
+
--- users/tests.py
+++ users/tests.py
@@ -1,4 +1,3 @@
-from django.test import TestCase
 from django.core.exceptions import ObjectDoesNotExist
 from django.test import TestCase
 from django.urls import reverse
@@ -7,29 +6,23 @@ from task_manager.utils import get_test_data, get_fixture_data
 
 
 class UserTests(TestCase):
-    fixtures = ['users.json']
+    # fixtures = ['users.json']
 
-    @classmethod
-    def setUpTestData(cls):
-        cls.test_data = get_test_data()
+    # @classmethod
+    # def setUpTestData(cls):
+    #     cls.test_data = get_test_data()
 
     def setUp(self):
-        user1 = {
-                'id': 1,
-                'username': 'MikeZh',
-                'first_name': 'Mike',
-                'password': '1570127'
-            }
-        self.user1 = User(**user1)
-        user2 = {
-                'id': 2,
-                'username': 'MarfaN',
-                'first_name': 'Marfa',
-                'password': '1234567'
-            }
-        self.user2 = User(**user2)
-        self.users = User.objects.bulk_create(
-            [self.user1, self.user2]
+        self.user1 = User.objects.create(
+            id=1,
+            username='MikeZh',
+            first_name='Mike',
+            password='1570127')
+        self.user2 = User.objects.create(
+            id=2,
+            username='MarfaN',
+            first_name='Marfa',
+            password='1234567'
         )
         self.users_list = reverse('users')
         self.login = reverse('login')
@@ -39,7 +32,7 @@ class UserTests(TestCase):
                           'password': '1570127',
                           'password2': '1570127'}
 
-    def test_users_view(self):
+    def test_user_view(self):
         response = self.client.get(reverse('users'))
         self.assertEqual(response.status_code, 200)
         self.assertContains(response, 'MikeZh')
@@ -52,4 +45,46 @@ class UserTests(TestCase):
         self.assertRedirects(response_post, self.login)
         self.assertTrue(User.objects.get(id=3))
 
+    def test_user_update(self):
+        update_url = reverse('update', args=(self.user1.id,))
+        response = self.client.get(update_url)
+        self.assertEqual(response.status_code, 200)
+        response_post = self.client.post(update_url, self.form_data)
+        self.assertRedirects(response_post, reverse('users'))
+        user = User.objects.get(pk=1)
+        self.assertEqual(user.username, self.form_data['username'])
 
+    def test_delete_user(self):
+        delete_url = reverse('delete', args=(self.user1.id,))
+        response = self.client.get(delete_url)
+        self.assertEqual(response.status_code, 200)
+        response_post = self.client.post(delete_url)
+        self.assertRedirects(response_post, reverse('users'))
+        with self.assertRaises(ObjectDoesNotExist):
+            User.objects.get(id=self.user1.id)
+
+    # def setUp(self):
+    #     user1 = {
+    #             'id': 1,
+    #             'username': 'MikeZh',
+    #             'first_name': 'Mike',
+    #             'password': '1570127'
+    #         }
+    #     self.user1 = User(**user1)
+    #     user2 = {
+    #             'id': 2,
+    #             'username': 'MarfaN',
+    #             'first_name': 'Marfa',
+    #             'password': '1234567'
+    #         }
+    #     self.user2 = User(**user2)
+    #     self.users = User.objects.bulk_create(
+    #         [self.user1, self.user2]
+    #     )
+    #     self.users_list = reverse('users')
+    #     self.login = reverse('login')
+    #     self.form_data = {'username': 'Woddie',
+    #                       'first_name': 'Woddie',
+    #                       'last_name': 'Allen',
+    #                       'password': '1570127',
+    #                       'password2': '1570127'}
