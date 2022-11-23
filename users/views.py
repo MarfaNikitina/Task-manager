a993423d1c104e311049c7bf420092b9732e2213
Marfa Nikitina <marfik93@mail.ru>
2022-11-23 12:10:37 +0300

    changing templates1

--- users/views.py
+++ users/views.py
@@ -1,5 +1,7 @@
 from django.contrib import messages
 from django.contrib.messages.views import SuccessMessageMixin
+from django.contrib.auth.mixins import LoginRequiredMixin,\
+    UserPassesTestMixin
 from django.urls import reverse_lazy
 # from django.contrib.auth.models import User
 from django.views.generic import ListView
@@ -11,32 +13,38 @@ from users.models import User
 
 class UserListView(ListView):
     model = User
-    template_name = 'users/user_list.html'
+    template_name = 'lists/user_list.html'
 
 
 class UserCreateView(SuccessMessageMixin, CreateView):
     form_class = UserRegistrationForm
     model = User
-    template_name = 'users/user_form.html'
+    template_name = 'create.html'
+    extra_context = {'title': _('Регистрация'),
+                     'button_text': _('Зарегистрировать')}
 
     def get_success_url(self):
         messages.success(self.request, _('Пользователь успешно зарегистрирован'))
         return reverse_lazy('login')
 
 
-class UserUpdateView(UpdateView):
+class UserUpdateView(LoginRequiredMixin, UpdateView):
     model = User
     form_class = UserRegistrationForm
-    template_name = 'users/user_update_form.html'
+    template_name = 'update.html'
+    extra_context = {'title': _('Изменение пользователя'),
+                     'button_text': _('Изменить')}
 
     def get_success_url(self):
         messages.success(self.request, _('Пользователь успешно изменён'))
         return reverse_lazy('users')
 
 
-class UserDeleteView(DeleteView):
+class UserDeleteView(LoginRequiredMixin, DeleteView):
     model = User
-    template_name = 'users/user_delete_form.html'
+    template_name = 'delete.html'
+    extra_context = {'title': _('Удаление пользователя')}
+    success_url = reverse_lazy('users')
 
     def get_success_url(self):
         messages.success(self.request, _('Пользователь успешно удален'))
